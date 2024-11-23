2d34f7df9144acf19bdd902843992fe5fde12c70
Christoffer Rehn <1280602+hoffa@users.noreply.github.com>
2024-11-23 15:50:05 +0900

    Simplify shell

--- docs/index.js
+++ docs/index.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmpp5tcmc94.js
+// include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmpgf89vwnt.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -217,21 +217,21 @@ Module['FS_createPath']("/src", "assets", true, true);
 
   })();
 
-// end include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmpp5tcmc94.js
-// include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmp8v3jyeho.js
+// end include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmpgf89vwnt.js
+// include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmp4oeuw3ie.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmp8v3jyeho.js
-// include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmp55tlsc4c.js
+  // end include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmp4oeuw3ie.js
+// include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmpb69ehj4c.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmp55tlsc4c.js
+  // end include: /var/folders/vz/1265_dp5667dmkwvmh2yj8nh0000gn/T/tmpb69ehj4c.js
 
 
 // Sometimes an existing Module object exists with properties
